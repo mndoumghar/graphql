3aff5dd6b5742f850d6fd5b34c67c0c49292754c
mndoumghar <mohmamedndoumghar@gmail.com>
2025-11-08 00:16:38 +0100

    add user info ...

--- components/UserInfo.js
+++ components/UserInfo.js
@@ -1,28 +1,32 @@
 import { Component } from '../core/Component.js';
 
 export class UserInfo extends Component {
-    constructor({ user}) {
+    constructor({ user }) {
         super();
         this.user = user || {};
     }
 
-      render() {
-        
-    const div = document.createElement('div');
-    div.classList.add('user-info');
+    render() {
 
-    const firstName = this.user.firstName || 'Unknown';
-    const lastName = this.user.lastName || '';
-    const login = this.user.login || 'username';
-    const avatar = this.user.avatarUrl || '';
-
-    div.innerHTML = `
-        <h2>${firstName} ${lastName}</h2>
-        <p>Username: ${login}</p>
-        <div id ="profile"><img src="${avatar}" alt="Avatar"/><div>
+        const div = document.createElement('div');
+        div.classList.add('user-info');
+        const firstName = this.user.firstName
+        const lastName = this.user.lastName
+        const username = this.user.login
+        const avatar = this.user.avatarUrl
+        const attrs = this.user.attrs
+        div.innerHTML = `
+         <div class="card-img">
+            <img src="${avatar}" alt="Avatar"/>
+        </div>
+                
+         <div class="card-info">
+            <h2>${username}</h2>
+            <p>${firstName} ${lastName}</p>
+        </div>
     `;
 
-    return div
-}
+        return div
+    }
 
 }
